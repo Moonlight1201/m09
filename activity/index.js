26437c02a2857b888ada8ba81ec9a76db20e679e
Ral Vjince Lomarda <ral.lomarda@gmail.com>
2023-05-09 22:10:23 +0800

    Add activity code

--- activity/index.js
+++ activity/index.js
@@ -1,56 +1,3 @@
-/*
-let favoriteVideoGame = {
-    Title: 'Honkai Star Rail',
-    Publisher: 'Mihoyo',
-    Year: 2023,
-    Price: "Free",
-    isAvailable: true
-}
-
-console.log(favoriteVideoGame)
-*/
-
-/*
-let instructor = {};
-
-instructor.age = 56;
-instructor.gender = "Male";
-instructor.department = "Humanities";
-instructor.courses = ['Philosophy 101', 'Humanities 101'],
-instructor.isActive = true;
-instructor.salary = 50000;
-
-console.log(instructor);
-*/
-
-/*
-function laptop (name, brand, price){
-    this.name = name;
-    this.brand = brand;
-    this.price = price;
-}
-
-let laptop1 = new laptop("MacBook Air w/ M2", "Apple", 66485);
-let laptop2 = new laptop("Legion Pro 7i", "Lenovo", 199987);
-
-console.log(laptop1);
-console.log(laptop2);
-*/
-
-/*
-function person(name,address,age){
-    this.name = name;
-    this.address = address;
-    this.age = age;
-    this.isGreeted = false;
-}
-
-let person1 = new person('Ral', 'Imus', 22);
-let person2 = new person('Vince','Imus', 22);
-
-console.log(person1);
-console.log(person2);
-*/
 
 let trainer= {
     Name: 'Moonlight',
@@ -65,7 +12,6 @@ let trainer= {
         console.log(`Pikachu! I choose you`)
     }
 }
-
 trainer.talk();
 
 function pokemon(Name,Level){
